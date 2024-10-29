4f8f09af679d524905964a18b6da51f1d2755479
jun99h1219 <jun99h1219@gmail.com>
2024-10-29 16:41:44 +0900

    edit orm

--- ormconfig.js
+++ ormconfig.js
@@ -4,7 +4,7 @@ module.exports = {
   "type": "mysql",
   // "host": process.env.DB_HOST,
   // "port": process.env.DB_PORT,
-  // "username": process.env.USER,
+  // "username": process.env.DB_USER,
   // "password": process.env.PASSWORD,
   // "database": process.env.DATABASE,
   "replication": {
